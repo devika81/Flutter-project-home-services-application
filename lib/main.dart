08632f86230646d2daad17e628f80d31c6c998ed
devika <devikakrishnant@gmail.com>
2023-12-11 23:51:10 +0530

    firebase connection done

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:home_services_app/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 //import 'package:home_services_app/seeker_or_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
